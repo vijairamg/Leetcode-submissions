7178bbd044cc8e3a9a6ebbd8aa64a9b879ac1f3e
vijairamg <vijairamg@gmail.com>
2023-08-21 18:17:42 -0400

    Time: 15 ms (83.26%), Space: 12.1 MB (59.37%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minDistance(string text1, string text2) {
+        vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        
+        for(int i = 1; i <= text1.size(); i++){
+            for(int j = 1; j <= text2.size(); j++){
+                                
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + dp[max(0,i-1)][max(0,j-1)];
+                }else{
+                    dp[i][j] = max(dp[i][max(j-1,0)],dp[max(i-1,0)][j]);
+                }
+            }
+        }
+                
+        return (text1.size()-dp[text1.size()][text2.size()]) + (text2.size()-dp[text1.size()][text2.size()]);
+    }
+};
