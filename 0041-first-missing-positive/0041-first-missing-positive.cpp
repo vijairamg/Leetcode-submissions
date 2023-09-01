11d12931f269b9ad45b282a7d9e6f74002a28b38
vijairamg <vijairamg@gmail.com>
2023-08-31 22:56:27 -0400

    Time: 123 ms (16.18%), Space: 41.2 MB (67.07%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        //Remove negatives
+        for(int i = 0; i < nums.size(); i++){
+            nums[i] = max(nums[i],0);
+        }
+        
+        for(int i = 0; i < nums.size(); i++){
+            int index = abs(nums[i])-1;
+            
+            if(index < 0 or index >= nums.size()){
+                continue;
+            }
+            
+            if(nums[index] == 0){
+                nums[index] = -(nums.size()+1);
+            }else{
+                nums[index] = -(abs(nums[index]));
+            }            
+        }
+        
+        for(int i = 0; i < nums.size(); i++){
+            cout<<nums[i]<<endl;
+        }
+        
+        for(int i = 1; i <= nums.size(); i++){
+            int index = i-1;
+            if(nums[index] >= 0){
+                return i;
+            }
+        }
+        
+        return nums.size()+1;
+    }
+};
