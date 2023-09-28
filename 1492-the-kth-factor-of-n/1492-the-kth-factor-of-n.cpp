8bde66018a3aaaadb4e31eb83a21503a5cdf5198
vijairamg <vijairamg@gmail.com>
2023-09-27 22:49:49 -0400

    Time: 0 ms (100.00%), Space: 6.3 MB (17.50%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+    
+        int j = 0;
+        
+        for(int i = 1; i <= n; i++){
+            if(n%i == 0){
+                j+=1;
+            }
+            
+            if(k == j){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
