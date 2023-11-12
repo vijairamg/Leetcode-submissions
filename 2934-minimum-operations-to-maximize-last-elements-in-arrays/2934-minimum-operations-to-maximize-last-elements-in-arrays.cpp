27d56d7e28ccb5e07b5ac4137c830dd3e8b2a52d
vijairamg <vijairamg@gmail.com>
2023-11-12 14:41:50 -0500

    Time: 31 ms (16.67%), Space: 42.9 MB (83.33%) - LeetHub

--- 2934-minimum-operations-to-maximize-last-elements-in-arrays/2934-minimum-operations-to-maximize-last-elements-in-arrays.cpp
+++ 2934-minimum-operations-to-maximize-last-elements-in-arrays/2934-minimum-operations-to-maximize-last-elements-in-arrays.cpp
@@ -26,19 +26,7 @@ public:
     int minOperations(vector<int>& nums1, vector<int>& nums2) {
         
         int n = nums1.size();
-        
-        int a = max_element(nums1.begin(),nums1.end()) - nums1.begin();
-        int b = max_element(nums2.begin(),nums2.end()) - nums2.begin();
-                
-        if(nums1[n-1] >= nums1[a] && nums2[n-1] >= nums2[b])
-            return 0;
-        
-        if(min(nums1[a],nums2[a]) > max(nums1[n-1],nums2[n-1]))
-            return -1;
-        
-        if(min(nums1[b],nums2[b]) > max(nums1[n-1],nums2[n-1]))
-            return -1;
-        
+
         int ans = getOperations(nums1,nums2);
         
         int temp = nums1[n-1];
